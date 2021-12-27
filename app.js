886e45841c4404433d1e170cc9fae3ef13a104a7
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-27 12:10:12 +0900

    getting each button

--- app.js
+++ app.js
@@ -3,4 +3,10 @@ let count = 0;
 
 const value = document.querySelector('#value');
 const buttons = querySelector('.btn');
-console.log(buttons);
+// console.log(buttons);
+
+buttons.forEach(function (button) {
+  button.addEventListener('click', function (event) {
+    console.log(event.currentTarget.classList);
+  });
+});
