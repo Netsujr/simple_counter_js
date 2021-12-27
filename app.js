4429374a9327972f67b25a6de202ad75c14fb3e3
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-27 12:14:06 +0900

    forEach function broke

--- app.js
+++ app.js
@@ -2,11 +2,16 @@
 let count = 0;
 
 const value = document.querySelector('#value');
-const buttons = querySelector('.btn');
+const buttons = document.querySelector('.btn');
 // console.log(buttons);
 
 buttons.forEach(function (button) {
   button.addEventListener('click', function (event) {
-    console.log(event.currentTarget.classList);
+    // console.log(event.currentTarget.classList);
+    const styles = event.currentTarget.classList;
+    if(styles.contains('decrease')) {
+      count--;
+    }
+    value.textContent = count;
   });
 });
